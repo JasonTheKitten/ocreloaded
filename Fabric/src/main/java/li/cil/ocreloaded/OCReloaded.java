ee206a8deece631bcf969e78673be781b1ea0f03
Jason Gronn <jasontopia03@gmail.com>
2024-02-28 23:20:53 -0500

    Add option to run via Fabric

---
+++ Fabric/src/main/java/li/cil/ocreloaded/OCReloaded.java
@@ -0,0 +1,12 @@
+package li.cil.ocreloaded;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OCReloaded implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        
+    }
+
+}
