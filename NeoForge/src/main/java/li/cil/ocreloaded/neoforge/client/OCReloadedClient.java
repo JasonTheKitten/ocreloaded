33233afd69fa57c7c8e654ee43237a8942161987
Jason Gronn <jasontopia03@gmail.com>
2024-04-28 15:29:33 -0400

    Add NeoForge

---
+++ NeoForge/src/main/java/li/cil/ocreloaded/neoforge/client/OCReloadedClient.java
@@ -0,0 +1,21 @@
+package li.cil.ocreloaded.forge.client;
+
+import li.cil.ocreloaded.minecraft.client.CommonClientHooks;
+import li.cil.ocreloaded.minecraft.client.registry.ClientRegistered;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class OCReloadedClient {
+
+    public OCReloadedClient(IEventBus eventBus) {
+        eventBus.register(this);
+    }
+
+    @SubscribeEvent
+    public void clientSetup(FMLClientSetupEvent event) {
+        ClientRegistered.setup();
+        CommonClientHooks.setup();
+    }
+
+}
---
+++ NeoForge/src/main/java/li/cil/ocreloaded/neoforge/common/OCReloaded.java
@@ -0,0 +1,33 @@
+package li.cil.ocreloaded.forge.common;
+
+
+import li.cil.ocreloaded.forge.client.OCReloadedClient;
+import li.cil.ocreloaded.minecraft.common.OCReloadedCommon;
+import li.cil.ocreloaded.minecraft.common.registry.CommonRegistered;
+import li.cil.ocreloaded.minecraft.server.CommonServerHooks;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+
+@Mod(OCReloadedCommon.MOD_ID)
+public class OCReloaded {
+
+    public OCReloaded(IEventBus eventBus) {
+        NeoForge.EVENT_BUS.register(this);
+        CommonRegistered.initialize();
+
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            new OCReloadedClient(eventBus);
+        }
+    }
+
+    @SubscribeEvent
+    public void serverStarted(ServerStartedEvent event) {
+        CommonServerHooks.setup(event.getServer());
+    }
+
+}
