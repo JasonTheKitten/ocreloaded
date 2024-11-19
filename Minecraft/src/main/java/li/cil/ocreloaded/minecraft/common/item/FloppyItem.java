26b84d8f7b1e8ca8d0bad2d1dd49460a2f13eb81
Jason Gronn <jasontopia03@gmail.com>
2024-11-19 13:03:40 -0500

    Add Floppy Textures

---
+++ Minecraft/src/main/java/li/cil/ocreloaded/minecraft/common/item/FloppyItem.java
@@ -0,0 +1,11 @@
+package li.cil.ocreloaded.minecraft.common.item;
+
+import net.minecraft.world.item.Item;
+
+public class FloppyItem extends Item {
+    
+    public FloppyItem(Properties properties) {
+        super(properties);
+    }
+
+}
